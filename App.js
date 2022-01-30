1ff51644915adfe9adfda4011d18cd280b23484a
chat14 <96359514+chat14@users.noreply.github.com>
2022-01-30 08:59:55 +0700

    App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const App = () => {
     <ChatEngine
       userName='jok99'//Put your userName instead
       projectID = 'cf39e29a-0e02-4ee4-b334-c5dbcab4314f'// Your project id goes here
-      userSecret=''// Replace with your secret key
+      userSecret='164e3177-3f11-404d-bd08-a5b8ef02881e'// Replace with your secret key
     />
   )
 }
