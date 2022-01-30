1e84e1e1ca5c9814157b93fadc35e5974c26cc7a
chat14 <96359514+chat14@users.noreply.github.com>
2022-01-30 09:18:32 +0700

    App.js

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import {ChatEngine} from 'react-chat-engine';
+import DirectMessaging from './DirectMessaging';
+import './App.css';
 
-//Using arrow function component
+
 const App = () => {
   return (
-    <ChatEngine
-      userName='jok99'//Put your userName instead
-      projectID = 'cf39e29a-0e02-4ee4-b334-c5dbcab4314f'// Your project id goes here
-      userSecret='164e3177-3f11-404d-bd08-a5b8ef02881e'// Replace with your secret key
-    />
+
+    <DirectMessaging/>
   )
 }
+
 export default App
