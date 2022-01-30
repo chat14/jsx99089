72aa3b1ebce65de74ec64a4890f3fb8c0390b6d3
chat14 <96359514+chat14@users.noreply.github.com>
2022-01-30 08:57:34 +0700

    App.js

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import {ChatEngine} from 'react-chat-engine';
 const App = () => {
   return (
     <ChatEngine
-      userName=''//Put your userName instead
-      projectID = ''// Your project id goes here
+      userName='jok99'//Put your userName instead
+      projectID = 'cf39e29a-0e02-4ee4-b334-c5dbcab4314f'// Your project id goes here
       userSecret=''// Replace with your secret key
     />
   )
